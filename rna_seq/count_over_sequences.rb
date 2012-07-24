5e725322e3d7ae478aa67809ead0f86a3160eda3
Jim Vallandingham <jfv@stowers.org>
2012-07-24 14:48:35 -0500

    use fastqc reader to find over rep

---
+++ rna_seq/count_over_sequences.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'fastqc_reader'
+require 'json'
+
+
+fastqc_reader = FastqcReader.new(ARGV[0],ARGV[1])
+
+data = fastqc_reader.data
+
+File.open("fasqc.json",'w') do |file|
+  file.puts JSON.pretty_generate(JSON.parse(data.to_json))
+end
+
+data.each do |name, data|
+  overrep_data = data["Overrepresented sequences"]
+  total = 0.0
+  overrep_data.each do |overrep|
+    per = overrep["Percentage"].to_f
+    total += per
+  end
+
+  puts "#{name}\t#{total}"
+end
+
